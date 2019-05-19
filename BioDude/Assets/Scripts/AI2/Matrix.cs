c19390cf9b9acd738efa5888735b3e5624ffb295
Margiris <34870816+Margiris@users.noreply.github.com>
2019-05-19 16:27:58 +0300

    a try on another NN

---
+++ BioDude/Assets/Scripts/AI2/Matrix.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = System.Random;
+
+public class Matrix : MonoBehaviour
+{
+    private int rows;
+    private int cols;
+    private double[,] matrix;
+    private Random rand;
+
+    public Matrix(int r, int c)
+    {
+        rows = r;
+        cols = c;
+        matrix = new double[rows, cols];
+
+        rand = new Random();
+    }
+
+    public void randomize()
+    {
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                matrix[i, j] = rand.Next(0, 200000) / 100000 - 1;
+            }
+        }
+    }
+
+    void output()
+    {
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                print(matrix[i, j] + "  ");
+            }
+
+            Console.WriteLine(" ");
+        }
+
+
+        Console.WriteLine();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
---
+++ BioDude/Assets/Scripts/AI2/NeuralNet.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NeuralNet : MonoBehaviour
+{
+    int iNodes;
+    int hNodes;
+    int oNodes;
+
+    // Matrix whi; //matrix containing weights between the input nodes and the hidden nodes
+    // Matrix whh; //matrix containing weights between the hidden nodes and the second layer hidden nodes
+    // Matrix woh; //matrix containing weights between the second hidden layer nodes and the output nodes
+    Matrix woi; //matrix containing weights between the input and the output nodes
+
+    public NeuralNet(int inputs, int hiddenCount, int outputCount)
+    {
+        iNodes = inputs;
+        oNodes = outputCount;
+        hNodes = hiddenCount;
+
+        woi = new Matrix(oNodes, iNodes + 1);
+
+        woi.randomize();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

241870b18703738b0f38817527b3ec6387e718eb
Margiris <34870816+Margiris@users.noreply.github.com>
2019-05-20 11:35:11 +0300

    add some code from Marius

---
+++ BioDude/Assets/Scripts/AI2/FinishPos.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishPos : MonoBehaviour
+{
+    public string playerTag = "Agent";
+    // Update is called once per frame
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.tag == playerTag)
+        {
+            col.gameObject.SetActive(false);
+            //proccess finish
+        }
+    }
+}
--- BioDude/Assets/Scripts/AI2/Population.cs
+++ BioDude/Assets/Scripts/AI2/Population.cs
@@ -4,13 +4,40 @@ using UnityEngine;
 
 public class Population : MonoBehaviour
 {
-    private const int playerCount = 100;
+    public int maxSteps = 10000;
+    public const int playerCount = 100;
+    public Transform playerPrefab;
+    public float mutationRate = 0.1f;
+
+    private Transform posFinish;
+    private Transform posStart;
+    private Transform populationFolder;
     private player[] players;
+    private int generation = 0;
+
+    private int agentCountCurrent;
+    private float fitnessSum = 0;
+    private int bestAgentIndex = 0;
+
+    private int stepCount = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        posFinish = GameObject.Find("PositionFinish").transform;
+        posStart = GameObject.Find("PositionStart").transform;
+        populationFolder = GameObject.Find("Population").transform;
+        players = new player[playerCount];
+
+        for (int i = 0; i < playerCount; i++)
+        {
+            players[i] = Instantiate(playerPrefab, populationFolder).GetComponent<player>();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Debug.Log(stepCount++);
     }
 
     // Update is called once per frame
